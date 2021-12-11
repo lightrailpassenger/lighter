b119ff07f9c98cad80c0db02cbabd70608c99071
Tony Chow <tonychow.0929@icloud.com>
2021-12-11 17:23:42 +0800

    Script for fetching data

---
+++ tools/fetchData/index.js
@@ -0,0 +1,78 @@
+(async () => {
+  const { default: fetch } = await import("node-fetch");
+  const { default: xml2js } = await import("xml-js");
+  const searchParams = new URLSearchParams();
+
+  searchParams.append(
+    'data',
+    "node(22.35, 113.9, 22.46, 114.1)['brand:en'='Light Rail']; out;"
+  );
+
+  const [
+    geoData,
+    routeData,
+  ] = await Promise.all([
+    fetch("https://overpass-api.de/api/interpreter", {
+      body: searchParams,
+      method: "POST",
+    }),
+    fetch("https://opendata.mtr.com.hk/data/light_rail_routes_and_stops.csv", {
+      method: "GET",
+    })
+  ]);
+  const [
+    geoDataText,
+    routeDataText,
+  ] = await Promise.all([
+    geoData.text(),
+    routeData.text(),
+  ]);
+  const geoDataObject = JSON.parse(xml2js.xml2json(geoDataText, { compact: true }));
+  const geoDataObjectTable = geoDataObject.osm.node.map((node) => {
+    return [
+      node._attributes.lat,
+      node._attributes.lon,
+      node.tag.find((tag) => (tag._attributes.k === "name:en"))._attributes.v,
+    ];
+  });
+
+  const routeTable = [];
+  for (let routeRow of routeDataText.split("\n").slice(1)) {
+    routeTable.push(routeRow.split(",").map((entry) => {
+      if (`${entry[0]}${entry[entry.length - 1]}` === '""') {
+        return entry.slice(1, -1);
+      } else {
+        return entry;
+      }
+    }));
+  }
+
+  const stations = [];
+  for (let [
+    route,
+    ,
+    ,
+    stationId,
+    nameCh,
+    nameEn,
+  ] of routeTable) {
+    stations.push({
+      id: stationId,
+      nameCh,
+      nameEn,
+      route,
+    });
+  }
+  const geoDataIndex = Object.create(null);
+  for (let [
+    lat,
+    lon,
+    nameEn
+  ] of geoDataObjectTable) {
+    geoDataIndex[nameEn] = { lat, lon };
+  }
+  for (let station of stations) {
+    Object.assign(station, geoDataIndex[station.nameEn]);
+  }
+  console.log(JSON.stringify(stations));
+})();
